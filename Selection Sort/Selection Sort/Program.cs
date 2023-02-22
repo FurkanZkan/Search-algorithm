3a1971da53426f82f44c55a599566153aacdcb5d
Furkan Özkan <furkan.105398@gmail.com>
2023-02-22 23:57:18 +0300

    Selection Sort

---
+++ Selection Sort/Selection Sort/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] arr = { 2, 3, 1, 8, -6, -9 };
+int temp = 0;
+
+
+for (int i = 0; i < arr.Length; i++)
+{
+    for (int j = i; j < arr.Length; j++)
+    {
+        if (arr[i] > arr[j])
+        {
+            temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
+
+foreach (int i in arr)
+{
+    Console.Write("{0}  ",i);
+}
