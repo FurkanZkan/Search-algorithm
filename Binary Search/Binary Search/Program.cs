d328bc0c086b38c61fe8044273c8d890e1c4d76d
Furkan Özkan <furkan.105398@gmail.com>
2023-02-27 20:38:21 +0300

    Binary Search
    Binary Search is an algorithm for finding a specific value in a sorted array. At each step in this technique, it is checked whether the sought value is equal to the middle value of the array.

---
+++ Binary Search/Binary Search/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] elements = new int[] { 4, 8, 5, 7, 2, 6, 10 };
+Console.WriteLine(binarySearch(elements, 10));
+
+
+int binarySearch(int[] elements, int x)
+{
+    int start = 0; 
+    int stop = elements.Length - 1;
+    int middle = (int)((start + stop) / 2);
+
+    while (elements[middle] != x  && start < stop )
+    {
+        if (x < elements[middle])
+            stop = middle - 1;
+        else
+            start = middle +1;
+
+
+        middle = (int)((start + stop) / 2); 
+    }
+    return (elements[middle] != x) ? -1 : middle;
+}
+
+
+
+  
